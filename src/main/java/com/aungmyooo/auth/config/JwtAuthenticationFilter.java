6a4f56c66623af47649ce1ea7362b787e171615c
Aung Myo Oo <dev.aungmyooo2015@gmail.com>
2024-01-12 09:01:25 +0800

    jwt setup done.

--- src/main/java/com/aungmyooo/auth/config/JwtAuthenticationFilter.java
+++ src/main/java/com/aungmyooo/auth/config/JwtAuthenticationFilter.java
@@ -1,11 +1,17 @@
 package com.aungmyooo.auth.config;
 
+import com.aungmyooo.auth.config.security.JwtService;
 import jakarta.annotation.Nonnull;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,7 +21,8 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-//    private final JwtService;
+    private final JwtService jwtService;
+    private UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(@Nonnull HttpServletRequest request, @Nonnull HttpServletResponse response, @Nonnull FilterChain filterChain) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
@@ -27,6 +34,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if(jwtService.isTokenValid(jwt, userDetails)){
+                UsernamePasswordAuthenticationToken authTOken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authTOken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authTOken);
+            }
+        }
+        filterChain.doFilter(request, response);
 
     }
 }
---
+++ src/main/java/com/aungmyooo/auth/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.aungmyooo.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @PostMapping("/register")
+    public ResponseEntity<> register(@RequestBody RegisterRequest request) {
+
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<> authenticate(@RequestBody RegisterRequest request) {
+
+    }
+}
